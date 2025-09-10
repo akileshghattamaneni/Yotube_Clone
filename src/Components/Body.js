be5d82ad2c7333a057012a0dae1f71ab8076c603
Sai Akilesh <akileshghattamaneni@gmail.com>
2025-09-10 22:33:09 +0530

    space updation

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import { Outlet } from 'react-router-dom'
-
-
+import React from "react";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
-    <div className='flex'>
+    <div className="flex">
       <Sidebar />
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
